b900ca1976566b43cc0600cc21512d211243ef12
Daniel Farah <danielfarah54@gmail.com>
2021-06-08 20:22:46 -0300

    home responsiva

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+// function openSidebar() {
+//     let sidebar = document.getElementById('sidebar')
+//     sidebar.style.width = '300px'
+// }
+
+// var btn_sidebar = document.getElementById('btn-sidebar')
+// btn_sidebar.addEventListener('click', openSidebar)
